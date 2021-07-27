d0992e06f7202c39d40eae1ec5d81eb3c9650d36
iiDeSTRoYeR <arabgamingnetwork@gmail.com>
2021-07-28 00:06:50 +0300

    initial commit

--- cmms/cmms/settings.py
+++ cmms/cmms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import os
+from secret import DjangoKey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q&ki=l@2eo)!4)f667#hdt*@@(untsvm4jes(3nldyzr7mwvbw'
+SECRET_KEY = DjangoKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
