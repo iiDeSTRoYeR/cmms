e62f2af34771e93afb8259ff1f867474c4491853
khlood <khlood1999@outlook.sa>
2021-08-03 23:58:37 +0300

    test

---
+++ cmms/supplier/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from .models import *
+from supplier.models import Supplier, Manufacturer, AgentCompany, Warranty
+
+
+class Add_Device_Warranty(forms.ModelForm):
+    DeviceAsset = forms.ModelchoiceField(
+        lable=mark_safe('<span style="font-style: italic;">DeviceAsset</span>'),
+        queryset=DeviceAsset.objects.all(),
+        required=True,
+        empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;">(Device Asset Number)</span>'),
+        widget=forms.Select(
+            attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'})
+
+    )
+
+
+class Warranty(forms.ModelForm):
+    Name = forms.CharField(
+        label='Manufacturer Name',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Samsung, Toshiba, etc...'})
+
+    Date =
+
+    Valid =
+
+    WarrantyPDF =
+
+    )
+
+class Companies(forms.ModelForm):
+    supplier = forms.ModelchoiceField(
+            lable=mark_safe('<span style="font-style: italic;">Supplier</span>'),
+            queryset=supplier.objects.all(),
+            required=True,
+            empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;"),
+            widget = forms.Select(
+            attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'})
+
+    Manufacturer = forms.ModelchoiceField(
+            lable=mark_safe('<span style="font-style: italic;">Manufacturer</span>'),
+            queryset=Manufacturer.objects.all(),
+            required=True,
+            empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;"),
+            widget = forms.Select(
+            attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'})
+
+    agentCompany = forms.ModelchoiceField(
+            lable=mark_safe('<span style="font-style: italic;">agentCompany</span>'),
+            queryset=agentCompany.objects.all(),
+            required=True,
+            empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;"),
+            widget = forms.Select(
+            attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'})
+
+        )
