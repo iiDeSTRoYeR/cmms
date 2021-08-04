7711d6c872efb3776ca9daa27bd994e888255a0c
khlood <khlood1999@outlook.sa>
2021-08-04 22:28:56 +0300

    test

--- cmms/supplier/views.py
+++ cmms/supplier/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views import View
+from django.views.generic import DeleteView, CreateView, ListView, DetailView, UpdateView
+from django.urls import reverse_lazy, reverse
+from django.utils.translation import gettext as _
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.views import redirect_to_login
+from inventory.models import DeviceAsset
+from .models import *
+from .forms import *
+from home.owner import UserAccessMixin
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World')
+
