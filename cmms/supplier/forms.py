6480f9b1799c98ead2f180a52a9960fdf9a9e7a8
khlood <khlood1999@outlook.sa>
2021-08-04 19:42:07 +0300

    test

--- cmms/supplier/forms.py
+++ cmms/supplier/forms.py
@@ -3,50 +3,51 @@ from .models import *
 from inventory.models import DeviceAsset
 from django.utils.html import mark_safe
 
-
-class AddDeviceWarranty(forms.ModelForm):
-    DeviceAssets =forms.ModelchoiceField(
+class ModelForm =(forms.ModelForm):
+    DeviceAssets=forms.ModelchoiceField(
         lable=mark_safe('<span style="font-style: italic;">DeviceAsset</span>'),
         queryset=DeviceAsset.objects.all(),
         required=True,
         empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;">(Device Asset Number)</span>'),
-        widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'}))
+        widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'}),)
 
 
 class Warranty(forms.ModelForm):
-    Name =forms.CharField(
+    Name = forms.CharField(
         label='Manufacturer Name',
         required=True,
         widget=forms.TextInput(attrs={'placeholder': 'Samsung, Toshiba, etc...'}))
 
-    Date =forms.DateField(
+    Date = forms.DateField(
         widget=forms.SelectDateWidget('%m/%d/%y'),
-        available=forms.BooleanField(label='Valid',))
+        available=forms.BooleanField(label='Valid', ))
 
-#class GeeksForm(forms.Form):
-       # Name =forms.CharField(
-        #geeks_field=forms.FileField(label='Warranty File',
-       # required=True,))
+    # class GeeksForm(forms.Form):
+    # Name =forms.CharField(
+    # geeks_field=forms.FileField(label='Warranty File',
+    # required=True,))
 
-
-class Companies(forms.ModelForm):
-    supplier =forms.ModelchoiceField(
+    class Companies(forms.ModelForm):
+        supplier = forms.ModelchoiceField(
             lable=mark_safe('<span style="font-style: italic;">Supplier</span>'),
             queryset=Supplier.objects.all(),
             required=True,
             empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;"'),
-            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'}))
+            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;',
+                                       'list': 'datalistOptions'}))
 
-    Manufacturer =forms.ModelchoiceField(
+        Manufacturer = forms.ModelchoiceField(
             lable=mark_safe('<span style="font-style: italic;">Manufacturer</span>'),
             queryset=Manufacturer.objects.all(),
             required=True,
             empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;'),
-            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'}))
+            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;',
+                                       'list': 'datalistOptions'}))
 
-    agentCompany =forms.ModelchoiceField(
+        agentCompany = forms.ModelchoiceField(
             lable=mark_safe('<span style="font-style: italic;">agentCompany</span>'),
             queryset=AgentCompany.objects.all(),
             required=True,
             empty_label=mark_safe('<span style="font-style: italic;color:#6c757d;"'),
-            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;', 'list': 'datalistOptions'}))
+            widget=forms.Select(attrs={'class': 'form-control', 'style': 'font-style: italic;color:#6c757d;',
+                                       'list': 'datalistOptions'}))
