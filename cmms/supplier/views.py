a5296bb7cbb256086446dd6c54e739d982502d53
khlood <khlood1999@outlook.sa>
2021-08-05 21:07:08 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cmms/supplier/forms.py

--- cmms/supplier/views.py
+++ cmms/supplier/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import *
 
 def index(request):
-    return HttpResponse('<h1>here suppliers <h1/>')
+    return HttpResponse('<h1>here suppliers </h1>')
 
 def CreateSupplierPage(request):
-    return HttpResponse('<h1>CreateSupplierPage-supplier <h1/>')
+    return HttpResponse('<h1>CreateSupplierPage-supplier </h1>')
